2ef3cb29f2a883fb17d606727c0ac0c151c1585b
MatiasCV27 <matiascriollo57@gmail.com>
2023-08-17 18:36:59 -0500

    Registo del propio usuario

--- PCDS2-Panaderia/Controllers/UsuariosController.cs
+++ PCDS2-Panaderia/Controllers/UsuariosController.cs
@@ -1,24 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PCDS2_Panaderia.Models;
 using PCDS2_Panaderia.Data;
-using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Authorization;
 
 namespace PCDS2_Panaderia.Controllers
 {
     public class UsuariosController : Controller
     {
         UsuariosData _userData = new UsuariosData();
+
+        [Authorize(Roles = "ADMIN")]
         public IActionResult ListarUser()
         {
             // La vista mostrara una Lista de Personas
             var oLista = _userData.ListaUsuarios();
             return View(oLista);
         }
+        [Authorize(Roles = "ADMIN")]
         public IActionResult Guardar()
         {
             // Metodo solo vuelve a la Vista
             return View();
         }
+        [Authorize(Roles = "ADMIN")]
         [HttpPost]
         public IActionResult Guardar(UsuariosModel oUser)
         {
@@ -33,11 +38,13 @@ namespace PCDS2_Panaderia.Controllers
             else
                 return View();
         }
+        [Authorize(Roles = "ADMIN")]
         public IActionResult Editar(int idUsuario)
         {
             var oUser = _userData.ObtenerUsuarios(idUsuario);
             return View(oUser);
         }
+        [Authorize(Roles = "ADMIN")]
         [HttpPost]
         public IActionResult Editar(UsuariosModel oUser)
         {
@@ -51,11 +58,13 @@ namespace PCDS2_Panaderia.Controllers
             else
                 return View();
         }
+        [Authorize(Roles = "ADMIN")]
         public IActionResult Eliminar(int idUsuario)
         {
             var oUser = _userData.ObtenerUsuarios(idUsuario);
             return View(oUser);
         }
+        [Authorize(Roles = "ADMIN")]
         [HttpPost]
         public IActionResult Eliminar(UsuariosModel oUser)
         {
@@ -63,6 +72,27 @@ namespace PCDS2_Panaderia.Controllers
 
             if (respuesta)
                 return RedirectToAction("ListarUser");
+            else
+                return View();
+        }
+
+        // Usuario Crea su propia cuenta
+        public IActionResult GuardarPropioUser()
+        {
+            // Metodo solo vuelve a la Vista
+            return View();
+        }
+        [HttpPost]
+        public IActionResult GuardarPropioUser(UsuariosModel oUser)
+        {
+            // Metodo recibe el objeto para guardarlo en BD
+            if (!ModelState.IsValid)
+                return View();
+
+            var respuesta = _userData.GuardarUsuarios(oUser);
+
+            if (respuesta)
+                return RedirectToAction("Login", "Acceso");
             else
                 return View();
         }
