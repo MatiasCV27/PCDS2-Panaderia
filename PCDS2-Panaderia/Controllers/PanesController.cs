e4e4da6734628a77d50133ef80bd41081b725001
MatiasCV27 <matiascriollo57@gmail.com>
2023-08-18 10:21:50 -0500

    Agregando carrito Part1

--- PCDS2-Panaderia/Controllers/PanesController.cs
+++ PCDS2-Panaderia/Controllers/PanesController.cs
@@ -6,22 +6,24 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace PCDS2_Panaderia.Controllers
 {
-    [Authorize(Roles = "ADMIN")]
     public class PanesController : Controller
     {
         PanesData _PanData = new PanesData();
 
+        [Authorize(Roles = "ADMIN")]
         public IActionResult Listar()
         {
             // La vista mostrara una Lista de Personas
             var oLista = _PanData.ListarPanes();
             return View(oLista);
         }
+        [Authorize(Roles = "ADMIN")]
         public IActionResult Guardar()
         {
             // Metodo solo vuelve a la Vista
             return View();
         }
+        [Authorize(Roles = "ADMIN")]
         [HttpPost]
         public IActionResult Guardar(PanesModel oPan)
         {
@@ -36,11 +38,13 @@ namespace PCDS2_Panaderia.Controllers
             else
                 return View();
         }
+        [Authorize(Roles = "ADMIN")]
         public IActionResult Editar(int idPanes)
         {
             var oPersona = _PanData.ObtenerPanes(idPanes);
             return View(oPersona);
         }
+        [Authorize(Roles = "ADMIN")]
         [HttpPost]
         public IActionResult Editar(PanesModel oPan)
         {
@@ -54,11 +58,13 @@ namespace PCDS2_Panaderia.Controllers
             else
                 return View();
         }
+        [Authorize(Roles = "ADMIN")]
         public IActionResult Eliminar(int idPanes)
         {
             var oPan = _PanData.ObtenerPanes(idPanes);
             return View(oPan);
         }
+        [Authorize(Roles = "ADMIN")]
         [HttpPost]
         public IActionResult Eliminar(PanesModel oPan)
         {
@@ -68,6 +74,13 @@ namespace PCDS2_Panaderia.Controllers
                 return RedirectToAction("Listar");
             else
                 return View();
+        }
+
+        // User Vista:
+        public IActionResult Ver_Panes()
+        {
+            var oLista = _PanData.ListarPanes();
+            return View(oLista);
         }
     }
 }
